6d7ccbce11f209cb0bba564cc1dc4c404f47cefc
Garner Perigo <tperigo3@gmail.com>
2020-10-28 13:25:59 -0500

    comment out non working code

--- script.js
+++ script.js
@@ -22,25 +22,25 @@ window.addEventListener("load", function() {
 
 // This block of code shows how to format the HTML once you fetch some planetary JSON!
 
-   fetch('https://handlers.education.launchcode.org/static/planets.json')
-       .then(function(response)
-           {return response.json()}
-       ).then(function(json){
-           const container = document.getElementById('container');
-           let planets = '';
-           for(planet of json){
-               planets += `
-                  <h2>Mission Destination</h2>
-                     <ol>
-                        <li>Name: ${json.name}</li>
-                        <li>Diameter: ${json.diameter}</li>
-                        <li>Star: ${json.star}</li>
-                        <li>Distance from Earth: ${json.distance}</li>
-                        <li>Number of Moons: ${json.moons}</li>
-                     </ol>
-                  <img src="${json[1].image}">
-               `;   
-           };
-           container.innerHTML = planets;
-       });     
+   // fetch('https://handlers.education.launchcode.org/static/planets.json')
+   //     .then(function(response)
+   //         {return response.json()}
+   //     ).then(function(json){
+   //         const container = document.getElementById('container');
+   //         let planets = '';
+   //         for(planet of json){
+   //             planets += `
+   //                <h2>Mission Destination</h2>
+   //                   <ol>
+   //                      <li>Name: ${json.name}</li>
+   //                      <li>Diameter: ${json.diameter}</li>
+   //                      <li>Star: ${json.star}</li>
+   //                      <li>Distance from Earth: ${json.distance}</li>
+   //                      <li>Number of Moons: ${json.moons}</li>
+   //                   </ol>
+   //                <img src="${json[1].image}">
+   //             `;   
+   //         };
+   //         container.innerHTML = planets;
+   //     });     
    });

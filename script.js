e791025aca107c057f543d7d9ebd84a1d50da2f2
Garner Perigo <tperigo3@gmail.com>
2020-10-28 13:20:48 -0500

    continuing on updating shuttle requirements

--- script.js
+++ script.js
@@ -2,18 +2,22 @@
 window.addEventListener("load", function() {
    let form = document.querySelector("form");
    form.addEventListener("submit", function(event) {
-      let pilotNameInput = document.querySelector("input[name=pilotName]");
+      let pilotName = document.querySelector("input[name=pilotName]");
       let copilotName = document.querySelector("input[name=copilotName]");
       let fuelLevel = document.querySelector("input[name=fuelLevel]");
       let cargoMass = document.querySelector("input[name=cargoMass]");
-      if (pilotNameInput.value === "" || copilotName.value === "" || fuelLevel.value === "" || cargoMass.Value === "") {
+      if (pilotName.value === "" || copilotName.value === "" || fuelLevel.value === "" || cargoMass.Value === "") {
          alert("All fields are required!");
          event.preventDefault();
+      }else if (isNaN(fuelLevel.value)=== true || isNaN(cargoMass.value)===true){
+         alert("Fuel Level and Cargo Mass must be numbers.");
+         event.preventDefault();
+      }else{
+         document.getElementsByClassName("#faultyItems").innerHTML = "visible";
+         document.getElementById("pilotStatus").innerHTML = `${pilotName} is ready.`;
+         document.getElementById("copilotStatus").innerHTML = `${copilotName} is ready.`;
       }
-      // if (isNaN(fuelLevel.value)=== true || isNaN(cargoMass.value)===true){
-      //    alert("Fuel Level and Cargo Mass must be numbers.");
-      //    event.preventDefault();
-      //}
+      
    });
 
 // This block of code shows how to format the HTML once you fetch some planetary JSON!

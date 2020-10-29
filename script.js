a5f2c3762ef198dc25f9a10e61e6ece6e13983ec
Garner Perigo <tperigo3@gmail.com>
2020-10-29 15:28:03 -0500

    finished project

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 window.addEventListener("load", function() {
    let form = document.querySelector("form");
    form.addEventListener("submit", function(event) {
-      event.preventDefault();
+      
       let pilotName = document.querySelector("input[name=pilotName]");
       let copilotName = document.querySelector("input[name=copilotName]");
       let fuelLevel = document.querySelector("input[name=fuelLevel]");
@@ -36,22 +36,23 @@ window.addEventListener("load", function() {
          }   
       }
      
-         fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response){
-            response.json().then(function(json) {
-               const missionLocation = document.getElementById("missionLocation");
-                  missionLocation.innerHTML =`
-                     <h2>Mission Destination</h2> 
-                     <ol>
-                        <li>Name: ${json[0].name}</li>
-                        <li>Diameter: ${json[0].diameter}</li>
-                        <li>Star: ${json[0].star}</li>
-                        <li>Distance from Earth: ${json[0].distance}</li>
-                        <li>Number of Moons: ${json[0].moons}</li>
-                     </ol>
-                     <img src="${json[0].image}">
-                  `;
-               });
+      fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response){
+         response.json().then(function(json) {
+            const missionLocation = document.getElementById("missionTarget");
+               missionLocation.innerHTML =`
+                  <h2>Mission Destination</h2> 
+                  <ol>
+                     <li>Name: ${json[0].name}</li>
+                     <li>Diameter: ${json[0].diameter}</li>
+                     <li>Star: ${json[0].star}</li>
+                     <li>Distance from Earth: ${json[0].distance}</li>
+                     <li>Number of Moons: ${json[0].moons}</li>
+                  </ol>
+                  <img src="${json[0].image}">
+               `;
             });
          });
+         event.preventDefault();
       });
+   });
   
